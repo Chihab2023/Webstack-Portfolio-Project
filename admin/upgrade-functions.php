cc6cb2db0defbc27d89c18031b813e57fdc20869
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-11-03 13:30:41 +0000

    user edit

--- admin/upgrade-functions.php
+++ admin/upgrade-functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * WordPress Upgrade Functions. Old file, must not be used. Include
+ * projectpilot Upgrade Functions. Old file, must not be used. Include
  * wp-admin/includes/upgrade.php instead.
  *
  * @deprecated 2.5.0
- * @package WordPress
+ * @package projectpilot
  * @subpackage Administration
  */
 
