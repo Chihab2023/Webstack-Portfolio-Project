0cd93e23d86b61af9a181fb22076c53ac67d9de2
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-11-03 13:39:00 +0000

    dependacy

--- backend/class-wp-error.php
+++ backend/class-wp-error.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * WordPress Error API.
+ * projectpilot Error API.
  *
- * @package WordPress
+ * @package projectpilot
  */
 
 /**
- * WordPress Error class.
+ * projectpilot Error class.
  *
- * Container for checking for WordPress errors and error messages. Return
+ * Container for checking for projectpilot errors and error messages. Return
  * WP_Error and use is_wp_error() to check if this class is returned. Many
- * core WordPress functions pass this class in the event of an error and
+ * core projectpilot functions pass this class in the event of an error and
  * if not handled properly will result in code errors.
  *
  * @since 2.1.0
