7846b33f48cffb8d24bb41ad5e27a0c5f75a0735
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-11-03 13:16:37 +0000

    modification commit

--- admin/about.php
+++ admin/about.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * About This Version administration panel.
- *
- * @package WordPress
- * @subpackage Administration
- */
 
-/** WordPress Administration Bootstrap */
+/** projectpilot Administration Bootstrap */
 require_once __DIR__ . '/admin.php';
 
 // Used in the HTML title tag.
-/* translators: Page title of the About WordPress page in the admin. */
+/* translators: Page title of the About projectpilot page in the admin. */
 $title = _x( 'About', 'page title' );
 
 list( $display_version ) = explode( '-', get_bloginfo( 'version' ) );
@@ -25,7 +19,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<?php
 					printf(
 						/* translators: %s: Version number. */
-						__( 'WordPress %s' ),
+						__( 'projectpilot %s' ),
 						$display_version
 					);
 					?>
@@ -47,7 +41,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p>
 					<?php
 					printf(
-						/* translators: 1: WordPress version number, 2: Plural number of bugs. */
+						/* translators: 1: projectpilot version number, 2: Plural number of bugs. */
 						_n(
 							'<strong>Version %1$s</strong> addressed %2$s bug.',
 							'<strong>Version %1$s</strong> addressed %2$s bugs.',
@@ -62,8 +56,8 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 						/* translators: %s: HelpHub URL. */
 						__( 'For more information, see <a href="%s">the release notes</a>.' ),
 						sprintf(
-							/* translators: %s: WordPress version. */
-							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							/* translators: %s: projectpilot version. */
+							esc_url( __( 'https://projectpilot.org/support/projectpilot-version/version-%s/' ) ),
 							sanitize_title( '6.6.2' )
 						)
 					);
@@ -72,7 +66,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p>
 					<?php
 					printf(
-						/* translators: 1: WordPress version number, 2: Plural number of bugs. */
+						/* translators: 1: projectpilot version number, 2: Plural number of bugs. */
 						_n(
 							'<strong>Version %1$s</strong> addressed %2$s bug.',
 							'<strong>Version %1$s</strong> addressed %2$s bugs.',
@@ -87,8 +81,8 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 						/* translators: %s: HelpHub URL. */
 						__( 'For more information, see <a href="%s">the release notes</a>.' ),
 						sprintf(
-							/* translators: %s: WordPress version. */
-							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							/* translators: %s: projectpilot version. */
+							esc_url( __( 'https://projectpilot.org/support/projectpilot-version/version-%s/' ) ),
 							sanitize_title( '6.6.1' )
 						)
 					);
@@ -103,13 +97,13 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<?php
 					printf(
 						/* translators: %s: Version number. */
-						__( 'Welcome to WordPress %s' ),
+						__( 'Welcome to projectpilot %s' ),
 						$display_version
 					);
 					?>
 				</h2>
 				<p class="is-subheading">
-					<?php _e( 'Create and deploy beautiful and coherent design elements across your sites with WordPress 6.6. A new rollback option for auto-updating plugins gives you control, flexibility, and peace of mind.' ); ?>
+					<?php _e( 'Create and deploy beautiful and coherent design elements across your sites with projectpilot 6.6. A new rollback option for auto-updating plugins gives you control, flexibility, and peace of mind.' ); ?>
 				</p>
 			</div>
 		</div>
@@ -181,7 +175,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<?php
 					printf(
 						/* translators: %1$s: code-formatted "WP_Theme_JSON", %2$s: code-formatted "data-wp-on-async", %%: escaped percent sign, leave as %%. */
-						__( 'WordPress 6.6 includes important updates like removing redundant %1$s calls, disabling autoload for large options, eliminating unnecessary polyfill dependencies, lazy loading post embeds, introducing the %2$s directive, and a 33%% reduction in template loading time in the editor.' ),
+						__( 'projectpilot 6.6 includes important updates like removing redundant %1$s calls, disabling autoload for large options, eliminating unnecessary polyfill dependencies, lazy loading post embeds, introducing the %2$s directive, and a 33%% reduction in template loading time in the editor.' ),
 						'<code>WP_Theme_JSON</code>',
 						'<code>data-wp-on-async</code>'
 					);
@@ -196,7 +190,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					</svg>
 				</div>
 				<h3><?php _e( 'Accessibility improvements' ); ?></h3>
-				<p><?php _e( '55+ accessibility fixes and enhancements focus on foundational aspects of the WordPress experience, particularly the data views component powering the new site editing experience and areas like the Inserter that provide a key way of interacting with blocks and patterns.' ); ?></p>
+				<p><?php _e( '55+ accessibility fixes and enhancements focus on foundational aspects of the projectpilot experience, particularly the data views component powering the new site editing experience and areas like the Inserter that provide a key way of interacting with blocks and patterns.' ); ?></p>
 			</div>
 		</div>
 
@@ -205,11 +199,11 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="about__section has-2-columns is-wider-left is-feature" style="background-color:var(--background);border-radius:var(--border-radius);">
 			<h3 class="is-section-header"><?php _e( 'And much more' ); ?></h3>
 			<div class="column">
-				<p><?php _e( 'For a comprehensive overview of all the new features and enhancements in WordPress 6.6, please visit the feature-showcase website.' ); ?></p>
+				<p><?php _e( 'For a comprehensive overview of all the new features and enhancements in projectpilot 6.6, please visit the feature-showcase website.' ); ?></p>
 			</div>
 			<div class="column aligncenter">
 				<div class="about__image">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/download/releases/6-6/' ) ); ?>" class="button button-primary button-hero"><?php _e( 'See everything new' ); ?></a>
+					<a href="<?php echo esc_url( __( 'https://projectpilot.org/download/releases/6-6/' ) ); ?>" class="button button-primary button-hero"><?php _e( 'See everything new' ); ?></a>
 				</div>
 			</div>
 		</div>
@@ -225,7 +219,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<?php
 					printf(
 						/* translators: %s: Version number. */
-						__( 'Learn more about WordPress %s' ),
+						__( 'Learn more about projectpilot %s' ),
 						$display_version
 					);
 					?>
@@ -233,10 +227,10 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p>
 					<?php
 					printf(
-						/* translators: 1: Learn WordPress link, 2: Workshops link. */
-						__( '<a href="%1$s">Learn WordPress</a> is a free resource for new and experienced WordPress users. Learn is stocked with how-to videos on using various features in WordPress, <a href="%2$s">interactive workshops</a> for exploring topics in-depth, and lesson plans for diving deep into specific areas of WordPress.' ),
-						'https://learn.wordpress.org/',
-						'https://learn.wordpress.org/online-workshops/'
+						/* translators: 1: Learn projectpilot link, 2: Workshops link. */
+						__( '<a href="%1$s">Learn projectpilot</a> is a free resource for new and experienced projectpilot users. Learn is stocked with how-to videos on using various features in projectpilot, <a href="%2$s">interactive workshops</a> for exploring topics in-depth, and lesson plans for diving deep into specific areas of projectpilot.' ),
+						'https://learn.projectpilot.org/',
+						'https://learn.projectpilot.org/online-workshops/'
 					);
 					?>
 				</p>
@@ -254,9 +248,9 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p style="margin-top:calc(var(--gap) / 2);">
 					<?php
 					printf(
-						/* translators: 1: WordPress Field Guide link, 2: WordPress version number. */
-						__( 'Explore the <a href="%1$s">WordPress %2$s Field Guide</a>. Learn about the changes in this release with detailed developer notes to help you build with WordPress.' ),
-						esc_url( __( 'https://make.wordpress.org/core/wordpress-6-6-field-guide/' ) ),
+						/* translators: 1: projectpilot Field Guide link, 2: projectpilot version number. */
+						__( 'Explore the <a href="%1$s">projectpilot %2$s Field Guide</a>. Learn about the changes in this release with detailed developer notes to help you build with projectpilot.' ),
+						esc_url( __( 'https://make.projectpilot.org/core/projectpilot-6-6-field-guide/' ) ),
 						'6.6'
 					);
 					?>
@@ -273,11 +267,11 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p style="margin-top:calc(var(--gap) / 2);">
 					<?php
 					printf(
-						/* translators: 1: WordPress Release Notes link, 2: WordPress version number. */
-						__( '<a href="%1$s">Read the WordPress %2$s Release Notes</a> for information on installation, enhancements, fixed issues, release contributors, learning resources, and the list of file changes.' ),
+						/* translators: 1: projectpilot Release Notes link, 2: projectpilot version number. */
+						__( '<a href="%1$s">Read the projectpilot %2$s Release Notes</a> for information on installation, enhancements, fixed issues, release contributors, learning resources, and the list of file changes.' ),
 						sprintf(
-							/* translators: %s: WordPress version number. */
-							esc_url( __( 'https://wordpress.org/documentation/wordpress-version/version-%s/' ) ),
+							/* translators: %s: projectpilot version number. */
+							esc_url( __( 'https://projectpilot.org/documentation/projectpilot-version/version-%s/' ) ),
 							'6-6'
 						),
 						'6.6'
@@ -324,24 +318,24 @@ __( 'Security Releases' );
 __( 'Maintenance and Security Release' );
 __( 'Maintenance and Security Releases' );
 
-/* translators: %s: WordPress version number. */
+/* translators: %s: projectpilot version number. */
 __( '<strong>Version %s</strong> addressed one security issue.' );
-/* translators: %s: WordPress version number. */
+/* translators: %s: projectpilot version number. */
 __( '<strong>Version %s</strong> addressed some security issues.' );
 
-/* translators: 1: WordPress version number, 2: Plural number of bugs. */
+/* translators: 1: projectpilot version number, 2: Plural number of bugs. */
 _n_noop(
 	'<strong>Version %1$s</strong> addressed %2$s bug.',
 	'<strong>Version %1$s</strong> addressed %2$s bugs.'
 );
 
-/* translators: 1: WordPress version number, 2: Plural number of bugs. Singular security issue. */
+/* translators: 1: projectpilot version number, 2: Plural number of bugs. Singular security issue. */
 _n_noop(
 	'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bug.',
 	'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bugs.'
 );
 
-/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
+/* translators: 1: projectpilot version number, 2: Plural number of bugs. More than one security issue. */
 _n_noop(
 	'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
 	'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.'
@@ -350,14 +344,14 @@ _n_noop(
 /* translators: %s: Documentation URL. */
 __( 'For more information, see <a href="%s">the release notes</a>.' );
 
-/* translators: 1: WordPress version number, 2: Link to update WordPress */
-__( 'Important! Your version of WordPress (%1$s) is no longer supported, you will not receive any security updates for your website. To keep your site secure, please <a href="%2$s">update to the latest version of WordPress</a>.' );
+/* translators: 1: projectpilot version number, 2: Link to update projectpilot */
+__( 'Important! Your version of projectpilot (%1$s) is no longer supported, you will not receive any security updates for your website. To keep your site secure, please <a href="%2$s">update to the latest version of projectpilot</a>.' );
 
-/* translators: 1: WordPress version number, 2: Link to update WordPress */
-__( 'Important! Your version of WordPress (%1$s) will stop receiving security updates in the near future. To keep your site secure, please <a href="%2$s">update to the latest version of WordPress</a>.' );
+/* translators: 1: projectpilot version number, 2: Link to update projectpilot */
+__( 'Important! Your version of projectpilot (%1$s) will stop receiving security updates in the near future. To keep your site secure, please <a href="%2$s">update to the latest version of projectpilot</a>.' );
 
-/* translators: %s: The major version of WordPress for this branch. */
-__( 'This is the final release of WordPress %s' );
+/* translators: %s: The major version of projectpilot for this branch. */
+__( 'This is the final release of projectpilot %s' );
 
-/* translators: The localized WordPress download URL. */
-__( 'https://wordpress.org/download/' );
+/* translators: The localized projectpilot download URL. */
+__( 'https://projectpilot.org/download/' );
