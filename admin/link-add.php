9c734269a91b26c15a5f3fbdd6e5fab6583070e2
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-11-03 13:24:25 +0000

    link

--- admin/link-add.php
+++ admin/link-add.php
@@ -2,11 +2,11 @@
 /**
  * Add Link Administration Screen.
  *
- * @package WordPress
+ * @package projectpilot
  * @subpackage Administration
  */
 
-/** Load WordPress Administration Bootstrap */
+/** Load projectpilot Administration Bootstrap */
 require_once __DIR__ . '/admin.php';
 
 if ( ! current_user_can( 'manage_links' ) ) {
